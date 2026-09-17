f1e0689e81d948a6cceba53448b7f26fef6a0aff
Leonard Walker <mr.leonard.walker@gmail.com>
2026-09-17 16:16:31 +0100

    Update Math Speed Blitz.py

--- Math Speed Blitz.py
+++ Math Speed Blitz.py
@@ -2,6 +2,8 @@ import random
 import time
 score=0
 fails=0
+streak=0
+highestStreak=0
 timeLimit=int(input("How many seconds to play do you want?"))
 timeBegin=time.time()
 while time.time()-timeBegin<timeLimit:
@@ -15,9 +17,19 @@ while time.time()-timeBegin<timeLimit:
         if userAnswer == answer:
                  score = score +1
                  print("Right!")
+                 streak = streak + 1
+                 if streak > 1:
+                     print(f"You're on a streak of {streak}!")
+                 if streak > highestStreak:
+                     highestStreak = streak
         else:
                  fails = fails +1
                  print("Wrong")
+                 if not streak == 0:
+                     print(f"You lost your streak of {streak}!")
+                 if streak > highestStreak:
+                     highestStreak = streak
+                 streak=0
     elif operatorReal==2:
         number1 = random.randint(-100, 100)
         number2 = random.randint(-100, 100)
@@ -27,9 +39,19 @@ while time.time()-timeBegin<timeLimit:
         if userAnswer == answer:
             score = score + 1
             print("Right!")
+            streak = streak + 1
+            if streak>1:
+                print(f"You're on a streak of {streak}!")
+            if streak>highestStreak:
+                highestStreak=streak
         else:
             fails=fails+1
             print("Wrong")
+            if not streak == 0:
+                print(f"You lost your streak of {streak}!")
+            if streak>highestStreak:
+                highestStreak=streak
+            streak=0
     elif operatorReal == 3:
          number1=random.randint(-12,12)
          number2=random.randint(-12,12)
@@ -39,9 +61,19 @@ while time.time()-timeBegin<timeLimit:
          if userAnswer == answer:
              score = score + 1
              print("Right!")
+             streak=streak+1
+             if streak > 1:
+                 print(f"You're on a streak of {streak}!")
+             if streak > highestStreak:
+                 highestStreak = streak
          else:
              fails = fails + 1
              print("Wrong!")
+             if not streak == 0:
+                 print(f"You lost your streak of {streak}!")
+             if streak > highestStreak:
+                 highestStreak = streak
+             streak=0
     elif operatorReal == 4:
         answer=random.randint(1,12)
         number2=random.randint(1,12)
@@ -51,22 +83,34 @@ while time.time()-timeBegin<timeLimit:
         if userAnswer == answer:
             score=score+1
             print("Correct!")
+            streak=streak+1
+            if streak>1:
+                print(f"You're on a streak of {streak}!")
+            if streak>highestStreak:
+                highestStreak=streak
         else:
             fails=fails+1
             print("Wrong!")
+            if streak>highestStreak:
+                highestStreak=streak
+            if not streak == 0:
+                print(f"You lost your streak of {streak}!")
+            streak=0
 print("Times up!")
 if score+fails > 0:
     print(f"You got {score} questions right!")
     print(f"You got {fails} questions wrong...")
     totalAnswered=score+fails
-    averageTime=timeLimit/totalAnswered
+    timeElapsed=time.time()-timeBegin
+    averageTime=timeElapsed/totalAnswered
+    averageTime=round(averageTime, 3)
     print(f"On average, you answered a question every {averageTime} seconds.")
     accuracy=score/totalAnswered
     accuracy=accuracy*100
     accuracy=round(accuracy, 2)
     print(f"You answered {accuracy}% questions correctly!")
+    print(f"Your highest streak was {highestStreak} questions correct in a row!")
 else:
     print("You didn't answer any questions!")
-
 
 
