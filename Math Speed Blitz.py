945ed272d41f9cfca35ba2169e1fb621b9793c62
Leonard Walker <mr.leonard.walker@gmail.com>
2026-09-17 15:54:06 +0100

    Update Math Speed Blitz.py

--- Math Speed Blitz.py
+++ Math Speed Blitz.py
@@ -6,27 +6,24 @@ timeLimit=int(input("How many seconds to play do you want?"))
 timeBegin=time.time()
 while time.time()-timeBegin<timeLimit:
     operatorReal=random.randint(1,4)
-    if operatorReal == 1 or operatorReal == 2:
+    if operatorReal == 1:
         number1=random.randint(-100,100)
         number2=random.randint(-100,100)
-        if operatorReal ==1:
-             answer=number1+number2
-             number11 = str(number1)
-             number22 = str(number2)
-             userAnswer1 = input(f"What is " + number11 + " plus " + number22 + "?")
-             userAnswer = int(userAnswer1)
-             if userAnswer == answer:
-                 score = score + 1
+        answer=number1+number2
+        userAnswer1 = input(f"What is {number1} plus {number2}?")
+        userAnswer = int(userAnswer1)
+        if userAnswer == answer:
+                 score = score +1
                  print("Right!")
-             else:
-                fails = fails + 1
-                print("Wrong")
         else:
-             answer=number1-number2
-             number11 = str(number1)
-             number22 = str(number2)
-             userAnswer1 =input(f"What is " + number11 + " minus " + number22 + "?")
-             userAnswer=int(userAnswer1)
+                 fails = fails +1
+                 print("Wrong")
+    elif operatorReal==2:
+        number1 = random.randint(-100, 100)
+        number2 = random.randint(-100, 100)
+        answer=number1-number2
+        userAnswer1 =input(f"What is {number1} minus {number2}?")
+        userAnswer=int(userAnswer1)
         if userAnswer == answer:
             score = score + 1
             print("Right!")
@@ -37,9 +34,7 @@ while time.time()-timeBegin<timeLimit:
          number1=random.randint(-12,12)
          number2=random.randint(-12,12)
          answer=number1*number2
-         number11 = str(number1)
-         number22 = str(number2)
-         userAnswer1 = input(f"What is " + number11 + " times " + number22 + "?")
+         userAnswer1 = input(f"What is {number1} times {number2}?")
          userAnswer = int(userAnswer1)
          if userAnswer == answer:
              score = score + 1
@@ -48,17 +43,10 @@ while time.time()-timeBegin<timeLimit:
              fails = fails + 1
              print("Wrong!")
     elif operatorReal == 4:
-        solvable="false"
-        while solvable=="false":
-            number1 = random.randint(1, 100)
-            number2 = random.randint(1, 12)
-            answer=number1/number2
-            #checking if question is solvable
-            if isinstance(answer, int):
-                solvable="true"
-        number11=str(number1)
-        number22=str(number2)
-        userAnswer1 =input(f"What is " + number11 + " divided by " + number22 + "?")
+        answer=random.randint(1,12)
+        number2=random.randint(1,12)
+        number1=number2*answer
+        userAnswer1 =input(f"What is {number1} divided by {number2}?")
         userAnswer=int(userAnswer1)
         if userAnswer == answer:
             score=score+1
@@ -67,19 +55,18 @@ while time.time()-timeBegin<timeLimit:
             fails=fails+1
             print("Wrong!")
 print("Times up!")
-scorePrint=str(score)
-print("You got "+ scorePrint+ " questions right!")
-failprint=str(fails)
-print("You got "+ failprint+" questions wrong...")
-totalAnswered=score+fails
-averageTime=totalAnswered/timeLimit
-averageTimePrint=str(averageTime)
-print("On average, you answered a question every "+ averageTimePrint + " seconds.")
-accuracy=totalAnswered/score
-failAccuracy=100-accuracy
-failAccuracyPrint=str(failAccuracy)
-accuracyPrint=str(accuracy)
-print("You answered "+ accuracyPrint + "% questions correctly and "+failAccuracyPrint+"% incorrectly")
+if score+fails > 0:
+    print(f"You got {score} questions right!")
+    print(f"You got {fails} questions wrong...")
+    totalAnswered=score+fails
+    averageTime=timeLimit/totalAnswered
+    print(f"On average, you answered a question every {averageTime} seconds.")
+    accuracy=score/totalAnswered
+    accuracy=accuracy*100
+    accuracy=round(accuracy, 2)
+    print(f"You answered {accuracy}% questions correctly!")
+else:
+    print("You didn't answer any questions!")
 
 
 
